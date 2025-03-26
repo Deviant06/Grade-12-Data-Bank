684a7a624417f82a451242260dfebac00402a5ae
ADMIN <julius.fabian06@gmail.com>
2025-03-26 18:40:03 +0800

    Fix routing and add missing dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Students from './pages/Students'
 import Import from './pages/Import'
+import Reports from './pages/Reports'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Students />} />
+        <Route path="/" element={<Navigate to="/students" replace />} />
         <Route path="/students" element={<Students />} />
         <Route path="/import" element={<Import />} />
+        <Route path="/reports" element={<Reports />} />
       </Routes>
     </Router>
   )
